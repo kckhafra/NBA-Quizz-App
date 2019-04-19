79fde020015248ea984cd93f05447cbb0a17e1b6
Kuntcheramen <kckhafra@gmail.com>
2019-04-19 09:54:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,151 @@
+
+let currentQuestion = 1;
+let score = 0;
+
+function generateQuestion() {
+  return `
+    <form class="form-questions">
+      <fieldset>
+        <legend>NBA Questions</legend>
+        <label class = "question"> ${STORE[currentQuestion-1].question} </label>
+          <ul>
+            <li> <input type="radio" value="${STORE[currentQuestion-1].answers[0]}" name="answers" required>
+              <span>${STORE[currentQuestion-1].answers[0]} </span>
+            </li>
+            <li> <input type="radio" value="${STORE[currentQuestion-1].answers[1]}" name="answers" required>
+              <span>${STORE[currentQuestion-1].answers[1]} </span>
+            </li>
+            <li> <input type="radio" value="${STORE[currentQuestion-1].answers[2]}" name="answers" required>
+              <span>${STORE[currentQuestion-1].answers[2]} </span>
+            </li>
+            <li> <input type="radio" value="${STORE[currentQuestion-1].answers[3]}" name="answers" required>
+              <span>${STORE[currentQuestion-1].answers[3]} </span>
+            </li>
+          </ul>
+        <button type="submit" value="submit" class= "submit-button">Submit</button>
+      </fieldset>
+    </form>`;   
+}
+
+function startQuiz() {
+  $('main').html(generateQuestion());
+}
+
+function handleEnterButton(){
+  $(".enter-button").on("click", function(){
+     updateQuestion()
+    startQuiz();
+  })
+}
+
+function handleSubmitButton() {
+    $("main").on("submit", function(){ 
+    event.preventDefault();
+   
+    handleUserAnswer()
+    
+  })
+}
+/*let correctAnswer = STORE[currentQuestion-1].answers[STORE[currentQuestion-1].correctAnswer]*/
+function handleUserAnswer() {
+  let selectedAnswer = $("input:checked").val();
+  console.log(selectedAnswer)
+  let correctAnswer = `${STORE[currentQuestion - 1].correctAnswer}`;
+  console.log(correctAnswer)
+  if (selectedAnswer===correctAnswer) {
+    ifAnswerCorrect() 
+  }
+  else {
+    ifAnswerWrong()
+  }
+}
+
+function ifAnswerCorrect(){
+  updateScore()
+  $("main").html(function(){
+    return `
+      <h2> CONGRATULATIONS</h2> 
+      <p> Your knowledge of basketball is at an all time high. <p>
+      <button value="Next" class= "next-button">Next</button>`
+  })
+}
+
+function ifAnswerWrong(){
+  $("main").html(function(){
+    return `
+      <h2> YOUR ANSWER IS WRONG </h2> 
+      <p> The correct answer is ${STORE[currentQuestion-1].correctAnswer}<p>
+      <button value="Next" class= "next-button">Next</button>`
+  })
+}
+
+function handleNextButton(){
+  $("main").on("click", '.next-button', function(){
+    if ( currentQuestion < STORE.length) { 
+    event.preventDefault();
+    iterateQuestion();
+    $('main').html(generateQuestion());
+    updateQuestion()
+    }
+   else {
+    results()
+  }
+  });
+}
+
+function iterateQuestion() {
+    currentQuestion++;
+  }
+function changeScore() {
+  score++;
+}
+
+function updateScore(){
+  changeScore()
+    $(".js-score").text(score)
+}
+
+function updateQuestion(){
+  currentQuestion+
+  $(".js-question").text(currentQuestion);
+}
+
+
+function results() {
+  $("main").html(function(){
+    if(score < 4) {
+    return `<h2 class = "end-score"> Your score is ${score}/10</h2> 
+    <p>You must study the game more. You can't be great unless you study the past greats.</p>
+      <button class= "restart-button">Restart</button>`
+    }
+    else if (score <= 6) {
+    return `<h2 class = "end-score"> Your score is ${score}/10</h2> 
+    <p>You are well on your way to be a expert of the game. Continue to study and you will reacch greatness.</p>
+      <button class= "restart-button">Restart</button>`
+    }
+
+    else  {
+    return `<h2 class = "end-score"> Your score is ${score}/10</h2> 
+    <p>You are a true student of the game. </p>
+      <button class= "restart-button">Restart</button>`
+    }
+
+  })
+}
+
+function handleRestartButton(){
+  $('main').on("click", ".restart-button", 
+  function() {
+    location.reload();
+    
+  })
+}
+
+function createNbaQuiz(){
+  handleEnterButton()
+  handleSubmitButton()
+  handleNextButton()
+  handleRestartButton()
+}
+
+$(createNbaQuiz());
---
+++ storage.js
@@ -0,0 +1,72 @@
+const STORE = [ 
+
+    { 
+      number: 1,
+      question: "Who is leading the NBA in 3pt fg?",
+      answers: ["Steph Curry", "James Harden", "Klay Thompson", "Damina Lilard"], 
+      correctAnswer: "James Harden"
+    },
+  
+    {
+      number: 2,
+      question: "Who won the NBA Coach of the Year award in 2019?",
+      answers: ["Nate McMillan", "Michael Malone", "Michael Budenholzer", "Doc Rivers"],
+      correctAnswer: "Michael Budenholzer"
+    },
+  
+    {  
+      number: 3,
+      question: "Who was the 2017-2018 NBA MVP?",
+      answers: ["Steph Curry", "Lebron James", "James Harden", "Russell Westbrook"],
+      correctAnswer: "James Harden"
+    },
+  
+    { 
+      number: 4, 
+      question: "Who is the all time leader in 3pt field goals?", 
+      answers: ["Reggie Miller", "Steph Curry", "James Harden", "Ray Allen"],
+      correctAnswer: "Ray Allen"
+    },
+  
+    { 
+      number: 5, 
+      question: "Who is the NBA GOAT?",
+      answers: ["Kobe Bryant", "Lebron James", "Michael Jordan", "Shaquille Oneal"],
+      correctAnswer: "Kobe Bryant"
+    },
+  
+    { 
+      number: 6,
+      question: "Who is the first person in NBA history to average a triple double?", 
+      answers: ["Oscar Roberson", "Lebron James", "Russell Westbrook", "Grant Hill"],
+      correctAnswer: "Oscar Roberson"
+    },
+  
+    {
+      number: 7,
+      question: "Who is the first person to average a triple double in 3 consecutive seasons?", 
+      answers: ["Oscar Roberson", "Lebron James", "Russell Westbrook", "Grant Hill"],
+      correctAnswer: "Russell Westbrook"
+    },
+  
+    {
+      number: 8,
+      question: "Who holds the record for most 3pt fg in a season?", 
+      answers: ["Reggie Miller", "Kyle Korver", "Steph Curry", "Klay Thompson"],
+      correctAnswer: "Steph Curry"
+    },
+      
+    { 
+      number: 9,
+      question: "Who holds the record for most 3pt fg in a game?",
+      answers: ["Reggie Miller", "Kyle Korver", "Steph Curry", "Klay Thompson"],
+      correctAnswer: "Klay Thompson"
+    },
+    
+    {  
+      number: 10,
+      question: "Who holds the all time record for most assist?",
+      answers: ["Chris Paul", "Mark Jackson", "Gary Payton", "John Stockton"],
+      correctAnswer: "John Stockton"
+    }
+  ]
